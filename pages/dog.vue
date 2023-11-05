928bc13b841c66a889e8ac5a848040febe07111f
watackeech <devkeech@gmail.com>
2023-11-05 17:42:25 +0900

    initial commit

---
+++ pages/dog.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="container">
+      <h1 class="title">I'm dog</h1>
+    </div>
+</template>
---
+++ pages/home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <h1 class="title">Hello World</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IndexPage'
+}
+</script>
